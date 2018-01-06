8fd1c25b0a6782ace4af0263728afadb14a6365a
zhangshimeng <zhangshimeng@autohome.com.cn>
2018-01-06 11:03:36 +0800

    commit 2

--- drag.js
+++ drag.js
@@ -1,5 +1,11 @@
 
 
-function drag(param) {  
+function drag(id) {  
     console.log('drag');
+    var obj = document.getElementById(id);
+    obj.onmousedown = function (e) { 
+        console.log('mousedown');
+    }
 }
+
+console.log(123);
